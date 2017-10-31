1bb389c8a0ddab7cbca50e2217eb1ff657884c50
William Parry <williamparry@gmail.com>
2017-10-31 16:16:00 +0100

    First cut of captureArea resizable

--- src/js/inject/captureArea.js
+++ src/js/inject/captureArea.js
@@ -1,74 +1,150 @@
-﻿var body = document.getElementsByTagName('body')[0],
-    overlay = document.createElement('div'),
-    isDragging = false,
-    startPos;
+﻿(function() {
 
-overlay.style.position = "absolute";
-overlay.style.width = "100%";
-overlay.style.height = "100%";
-overlay.style.zIndex = "999999999";
-overlay.style.cursor = "crosshair";
+    var body = document.getElementsByTagName('body')[0],
+        overlay = document.createElement('div'),
+        isDragging = false,
+        isResizing = false,
+        startPos;
 
-var dragArea = document.createElement('div');
-dragArea.style.backgroundColor = "#555";
-dragArea.style.opacity = "0.25";
-dragArea.style.position = "absolute";
-dragArea.style.display = "block";
-dragArea.style.width = 0;
-dragArea.style.border = "1px solid #000";
+    overlay.style.position = "absolute";
+    overlay.style.width = "100%";
+    overlay.style.height = "100%";
+    overlay.style.zIndex = "999999999";
+    overlay.style.cursor = "crosshair";
 
-overlay.appendChild(dragArea);
-body.insertBefore(overlay, body.firstChild);
-body.prevOverflow = body.style.overflowY;
-body.style.overflowY = 'hidden';
-overlay.style.top = window.pageYOffset + 'px';
+    var dragArea = document.createElement('div');
+    dragArea.style.backgroundColor = "rgba(0,0,0,0.2)";
+    dragArea.style.position = "absolute";
+    dragArea.style.display = "block";
+    dragArea.style.resize = "both";
+    dragArea.style.overflow = "auto";
+    dragArea.style.width = 0;
+    dragArea.style.border = "1px solid rgba(0,0,0,0.5)";
+    dragArea.style.boxSizing = "border-box";
+    overlay.appendChild(dragArea);
 
-overlay.onmousedown = function (e) {
-    isDragging = true;
-    startPos = [e.clientX, e.clientY];
-    dragArea.style.left = startPos[0] + 'px';
-    dragArea.style.top = startPos[1] + 'px';
-};
+    var tools = document.createElement('div');
+    tools.style.position = "absolute";
+    tools.style.top = "10px";
+    tools.style.right = "10px";
+    dragArea.appendChild(tools);
 
-overlay.onmousemove = function (e) {
+    var okButton = document.createElement('span');
+    okButton.style.padding = "5px 10px";
+    okButton.style.marginRight = "10px";
+    okButton.style.background = "rgba(0,128,0,0.5)";
+    okButton.style.cursor = "pointer";
+    okButton.style.display = "inline-block";
+    okButton.style.boxShadow = "0 0 5px #000";
+    okButton.style.color = "#fff";
+    okButton.style.border = dragArea.style.border;
+    okButton.innerHTML = "✓";
+    tools.appendChild(okButton);
 
-    if (isDragging) {
+    var cancelButton = document.createElement('span');
+    cancelButton.style.padding = "5px 10px";
+    cancelButton.style.background = "rgba(255,0,0,0.5)";
+    cancelButton.style.cursor = "pointer";
+    cancelButton.style.display = "inline-block";
+    cancelButton.style.boxShadow = "0 0 5px #000";
+    cancelButton.style.color = "#fff";
+    cancelButton.style.border = dragArea.style.border;
+    cancelButton.innerHTML = "✕";
+    tools.appendChild(cancelButton);
 
-        var posx = e.clientX;
-        var posy = e.clientY;
+    body.insertBefore(overlay, body.firstChild);
+    body.prevOverflow = body.style.overflowY;
+    body.style.overflowY = 'hidden';
+    overlay.style.top = window.pageYOffset + 'px';
 
-        if (posx < startPos[0]) {
-            dragArea.style.left = posx + 'px';
-        }
-        if (posy < startPos[1]) {
-            dragArea.style.top = posy + 'px';
-        }
-
-        dragArea.style.width = Math.max(posx, startPos[0]) - Math.min(posx, startPos[0]) + 'px';
-        dragArea.style.height = Math.max(posy, startPos[1]) - Math.min(posy, startPos[1]) + 'px';
-    }
-
-};
-
-overlay.onmouseup = function (e) {
-    if (isDragging) {
-    	chrome.runtime.sendMessage({
-            CMD: 'got_area',
-            Data: {
-                left:   dragArea.style.left.split('px')[0],
-                top:    dragArea.style.top.split('px')[0],
-                width:  dragArea.style.width.split('px')[0],
-                height: dragArea.style.height.split('px')[0]
-            }
-        });
-        
-        overlay.style.display = 'none';
-        isDragging = false;
-
+    function resetBody() {
         body.style.overflowY = body.prevOverflow;
+    }
+    function removeOverlay() {
         body.removeChild(overlay);
-
     }
 
-};
+    dragArea.onmouseover = function() {
+        if(!isResizing) {
+            tools.style.display = "block";
+        }
+    }
 
+    dragArea.onmouseup = function() {
+        isResizing = false;
+    }
+
+    dragArea.onmousedown = function() {
+        isResizing = true;
+        tools.style.display = "none";
+    }
+
+    dragArea.onmouseout = function() {
+        tools.style.display = "none";
+    }
+
+    cancelButton.onmousedown = function(e) {
+        e.stopPropagation();
+        removeOverlay();
+        resetBody();
+    }
+
+    okButton.onmousedown = function(e) {
+        e.stopPropagation();
+        submit();   
+    };
+
+    overlay.onmousedown = function (e) {
+        isDragging = true;
+        startPos = [e.clientX, e.clientY];
+        dragArea.style.left = startPos[0] + 'px';
+        dragArea.style.top = startPos[1] + 'px';
+    };
+
+    overlay.onmousemove = function (e) {
+
+        if (isDragging) {
+
+            var posx = e.clientX;
+            var posy = e.clientY;
+
+            if (posx < startPos[0]) {
+                dragArea.style.left = posx + 'px';
+            }
+            if (posy < startPos[1]) {
+                dragArea.style.top = posy + 'px';
+            }
+
+            dragArea.style.width = Math.max(posx, startPos[0]) - Math.min(posx, startPos[0]) + 'px';
+            dragArea.style.height = Math.max(posy, startPos[1]) - Math.min(posy, startPos[1]) + 'px';
+        }
+
+    };
+
+    overlay.onmouseup = function (e) {
+        tools.style.display = "block";
+        overlay.onmousedown = null;
+        overlay.onmousemove = null;
+        overlay.style.cursor = "default";
+    };
+
+    function submit() {
+
+        var obj = {
+            left: parseInt(dragArea.style.left),
+            top: parseInt(dragArea.style.top),
+            width: parseInt(dragArea.style.width),
+            height: parseInt(dragArea.style.height)
+        }
+
+        removeOverlay();
+
+        chrome.runtime.sendMessage({
+            CMD: 'got_area',
+            Data: obj
+        });
+
+        setTimeout(resetBody, 50);
+    }
+
+}());
